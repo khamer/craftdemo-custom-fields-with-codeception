4f83da9587c3d46950880f6e7362d8f95291cad5
Kevin Hamer <kevin@imarc.com>
2022-02-08 10:08:15 -0500

    ignore output; example failing neo test

--- tests/functional/ExampleCest.php
+++ tests/functional/ExampleCest.php
@@ -42,4 +42,43 @@ class ExampleCest
         $I->amOnPage("/blog/example-blog-article");
         $I->see("Here is some");
     }
+
+    public function testNeo(FunctionalTester $I)
+    {
+        $section = Craft::$app->sections->getSectionByHandle('blog');
+        $entryType = current($section->getEntryTypes());
+        $author = User::find()->admin()->one();
+
+        $entry = new Entry([
+            'sectionId' => $section->id,
+            'typeId' => $entryType->id,
+            'fieldLayoutId' => $entryType->fieldLayoutId,
+            'authorId' => $author->id,
+            'title' => 'Another Blog Article',
+            'slug' => 'another-blog-article',
+            'postDate' => new DateTime(),
+        ]);
+
+        $entry->setFieldValue('neoContent',
+            [
+                'blocks' => [
+                    'new1' => [
+                        'modified' => 1,
+                        'type' => 'richText',
+                        'level' => 0,
+                        'enabled' => true,
+                        'collapsed' => false,
+                        'fields' => [
+                            'neoRichText' => "Here is more <em>example</em> <strong>rich</strong> text.",
+                        ],
+                    ],
+                ],
+            ],
+        );
+
+        $I->saveElement($entry);
+
+        $I->amOnPage("/blog/another-blog-article");
+        $I->see("Here is more");
+    }
 }
